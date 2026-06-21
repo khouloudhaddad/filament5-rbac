ed6a1fc507a151eaffbb8099e369129f33e3e1ff
Khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2026-06-21 20:05:59 +0100

    Create models, relationships and filament resources

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('user_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+                Toggle::make('is_published')
+                    ->required(),
+            ]);
+    }
+}
